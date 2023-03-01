c2bb0120e9b5a0f26af89f372cbfaedaaf005cc2
maysaa1992 <batainehmaysaa@gmail.com>
2023-03-01 18:36:48 +0000

    ubdate code

--- app.js
+++ app.js
@@ -3,24 +3,23 @@
 
 var name =prompt("whats your name");
 var gender =prompt("whats your gender male or female ");
-if ( gender != "male" && gender!="female"){
-    var welcom =confirm("are you wants to show the welcoming message")
-    if (welcom==true){alert("welcom "+ name );} 
-}
-else if (gender == 'male' ){
+
+
+if (gender.toLowerCase()== 'male' ){
     var welcom =confirm("are you wants to show the welcoming message")
 if (welcom==true){alert("welcom Mr "+ name );}  
 }
-else if (gender == 'female' ){  
+else if (gender.toLowerCase() == 'female' ){  
         var welcom =confirm("are yo wants to skip the welcoming message")
 if (welcom==false){alert("welcom Ms "+ name );}  
 }
+else if ( gender != "male" && gender!="female"){
+    var welcom =confirm("are you wants to show the welcoming message")
+    if (welcom==true){alert("welcom "+ name );} }
 
-
-do {
-    var age =prompt("whats your age ");
-    if (age<=0){alert("the age is less than or equal to zero plese input your age agine")}
-  } while (age <=0);
-
+// do {
+//     var age =prompt("whats your age ");
+//     if (age<=0){alert("the age is less than or equal to zero plese input your age agine")}
+//   } while (age <=0);
 
 
