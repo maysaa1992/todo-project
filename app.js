0b78417a6beae7b07419db8f4a242a6ca19de195
maysaa1992 <batainehmaysaa@gmail.com>
2023-03-01 08:51:03 +0000

    add js file

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
 
 
-var welcom =confirm("are yo wants to skip the welcoming message")
-if (welcom==false){
+
 var name =prompt("whats your name");
 var gender =prompt("whats your gender male or female ");
-if (gender == 'male' ){
-    alert("welcom Mr"+ name );
+if ( gender != "male" && gender!="female"){
+    var welcom =confirm("are yo wants to skip the welcoming message")
+    if (welcom==false){alert("welcom "+ name );} 
 }
-else {
-    if (gender == 'female' ){
-        alert("welcom Ms"+ name );
+else if (gender == 'male' ){
+    var welcom =confirm("are yo wants to skip the welcoming message")
+if (welcom==false){alert("welcom Mr "+ name );}  
 }
-else{ alert("welcom " + name )}
+else if (gender == 'female' ){  
+        var welcom =confirm("are yo wants to skip the welcoming message")
+if (welcom==false){alert("welcom Ms "+ name );}  
 }
+
+
     
 var age =prompt("whats your age ");
 if (age<=0){alert("the age is less than or equal to zero")}
 
-}
 
 
 
 
 
-  /*  var age =prompt("whats your age ");
-    if (age>0 && age!=0){ alert("welcom")}
-    else {alert("plese check your age ");
-    var age =prompt("whats your age ");
-    if (age>0 && age!=0){alert("welcom")}
-}
-}
-else {alert("plese enter male or female");
-
-var gender =prompt("whats your gender male or female ");
-if (gender == 'male' || gender == 'female'){  
-    var age =prompt("whats your age ");
-    if (age>0 && age!=0){  alert("wlcome ")}
-    else {alert("plese check your age");
-    var age =prompt("whats your age ");
-    if (age>0 && age!=0){alert("welcom")}
-}
-}
-}*/
 
