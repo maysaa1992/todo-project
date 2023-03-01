34bab4b328aefab398a08c23cbda885fb67d545e
maysaa1992 <batainehmaysaa@gmail.com>
2023-03-01 09:31:05 +0000

    add do while

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 var name =prompt("whats your name");
 var gender =prompt("whats your gender male or female ");
 if ( gender != "male" && gender!="female"){
-    var welcom =confirm("are yo wants to skip the welcoming message")
-    if (welcom==false){alert("welcom "+ name );} 
+    var welcom =confirm("are you wants to show the welcoming message")
+    if (welcom==true){alert("welcom "+ name );} 
 }
 else if (gender == 'male' ){
-    var welcom =confirm("are yo wants to skip the welcoming message")
-if (welcom==false){alert("welcom Mr "+ name );}  
+    var welcom =confirm("are you wants to show the welcoming message")
+if (welcom==true){alert("welcom Mr "+ name );}  
 }
 else if (gender == 'female' ){  
         var welcom =confirm("are yo wants to skip the welcoming message")
@@ -17,13 +17,10 @@ if (welcom==false){alert("welcom Ms "+ name );}
 }
 
 
-    
-var age =prompt("whats your age ");
-if (age<=0){alert("the age is less than or equal to zero")}
-
-
-
-
+do {
+    var age =prompt("whats your age ");
+    if (age<=0){alert("the age is less than or equal to zero plese input your age agine")}
+  } while (age <=0);
 
 
 
